53bbbe15a6ea58dd0c90463735cc8a2dfad520cf
Administrator <Administrator@kevin>
2018-08-29 20:42:56 +0800

    commit1

--- Spring-mybatis/src/main/java/com/yc/dao/AccountMapper.java
+++ Spring-mybatis/src/main/java/com/yc/dao/AccountMapper.java
@@ -5,7 +5,6 @@ import com.yc.po.Account;
 
 public interface AccountMapper {
     int deleteByPrimaryKey(Integer accountid);
-
     int insert(Account record);
 
     int insertSelective(Account record);
